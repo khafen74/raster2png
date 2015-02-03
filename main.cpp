9748aef04d2497a81ec91e5248d958e03f61be9b
Konrad Hafen <khafen74@gmail.com>
2015-02-03 08:24:40 -0700

    Introducing a tag for the first release

--- main.cpp
+++ main.cpp
@@ -14,20 +14,24 @@ int main(int argc, char *argv[])
 
     GDALAllRegister();
 
-    const char *inputPath = "C:/Test/z_crap/ras2png/dem1.tif";
-    const char *pngPath = "C:/Test/z_crap/ras2png/z_ras2png_out/dem1.png";
-    const char *inPath2 = "C:/Test/z_crap/ras2png/hlsd1.tif";
-    const char *pPath2 = "C:/Test/z_crap/ras2png/z_ras2png_out/hlsd1.png";
-    const char *list = "C:/Test/z_crap/ras2png/z_ras2png_out/hlsd1.png;C:/Test/z_crap/ras2png/z_ras2png_out/dem1.png;C:/Test/z_crap/ras2png/z_ras2png_out/dem1_legend.png";
-    const char *listOut = "C:/Test/z_crap/ras2png/z_ras2png_out/stacked.png";
+    const char *inputPath = "C:/Test/z_crap/ras2png/error4.tif";
+    const char *pngPath = "C:/Test/z_crap/ras2png/z_ras2png_out/error4.png";
+    const char *inPath2 = "C:/Test/z_crap/ras2png/hlsd7.tif";
+    const char *pPath2 = "C:/Test/z_crap/ras2png/z_ras2png_out/hlsd7.png";
+    const char *list = "C:/Test/z_crap/ras2png/z_ras2png_out/error4.png;C:/Test/z_crap/ras2png/z_ras2png_out/error4_legend.png";
+    const char *listOut = "C:/Test/z_crap/ras2png/z_ras2png_out/error4stacked.png";
 
-    Renderer_StretchMinMax *Renderer1 = new Renderer_StretchMinMax(inputPath, CR_DEM, 128, false, false);
+//    Renderer_StretchMinMax *Renderer2 = new Renderer_StretchMinMax(inputPath, CR_DEM, 128);
+//    Renderer2->rasterToPNG(pngPath, 100, 1000);
+//    Renderer2->setPrecision(2);
+//    Renderer2->printLegend();
+    Renderer_GCDError *Renderer1 = new Renderer_GCDError(inputPath);
     Renderer1->rasterToPNG(pngPath, 100, 1000);
-    Renderer1->setPrecision(2);
+    //Renderer1->setPrecision(2);
     Renderer1->printLegend();
 
-    Renderer_ByteData *Renderer2 = new Renderer_ByteData(inPath2);
-    Renderer2->rasterToPNG(pPath2, 100, 1000);
+    //Renderer_ByteData *Renderer2 = new Renderer_ByteData(inPath2);
+    //Renderer2->rasterToPNG(pPath2, 100, 1000);
 
     Renderer::stackImages(list, listOut, 100);
 
